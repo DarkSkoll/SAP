3a27435099344dab2321ff07998656edadde6435
darkskoll <erick.lievana@gmail.com>
2020-11-26 14:42:32 -0600

    Create Window Class and Add it to Launcher
    Add a Window class with title and a table.
    TODO: Fix table grid

---
+++ Launcher.java
@@ -0,0 +1,7 @@
+public class Launcher{
+
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.createWindow();
+    }
+}
---
+++ Window.java
@@ -0,0 +1,50 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.Font;
+
+public class Window{
+    private JFrame window;
+    private JTable table;
+    private JLabel title;
+
+    public Window(){
+        window = new JFrame();
+        table = new JTable(6,20);
+        title = new JLabel("Simulador de Algoritmo de Planificación SAP");
+    }
+
+    private void setDefaultWindowsProperties(){
+        window.setSize(1600,900);
+        window.setLayout(null);
+        window.setVisible(true);
+        window.getContentPane().setBackground(Color.black);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void setDefaultTableProperties(){
+        table.setBounds(20,110,1560,300);
+        table.setEnabled(false);
+    }
+
+    private void setDefaultTitleProperties(){
+        title.setBounds(0,20,1600,100);
+        title.setForeground(Color.white);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+        title.setFont(new Font("Mono", Font.BOLD, 36));
+    }
+
+    private void addWindowsElements(){
+        window.add(title);
+        window.add(table);
+    }
+
+    public void createWindow(){
+        setDefaultWindowsProperties();
+        setDefaultTableProperties();
+        setDefaultTitleProperties();
+        addWindowsElements();
+    }
+}
