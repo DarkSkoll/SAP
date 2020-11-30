435ef7b5b77bd1a98c8a74e9b471f75f08491780
darkskoll <erick.lievana@gmail.com>
2020-11-30 17:14:07 -0600

    Add table

--- Window.java
+++ Window.java
@@ -1,18 +1,31 @@
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Font;
 
 public class Window{
     private JFrame window;
-    private JTable table;
     private JLabel title;
+    private JLabel mensajeUltimo;
+    private JLabel mensajeCola;
+    private JLabel mensajeActual;
+    private JLabel procesoUltimo;
+    private JLabel procesoCola;
+    private JLabel procesoActual;
+    private JTable calculos;
 
     public Window(){
         window = new JFrame();
-        table = new JTable(6,20);
         title = new JLabel("Simulador de Algoritmo de Planificación SAP");
+        mensajeUltimo = new JLabel("Ultimo Proceso en Llegar: ");
+        mensajeCola = new JLabel("Siguiente Proceso en Pasar: ");
+        mensajeActual = new JLabel("Proceso Actual: ");
+        procesoUltimo = new JLabel();
+        procesoCola = new JLabel();
+        procesoActual = new JLabel();
+        calculos = new JTable(8,10);
     }
 
     private void setDefaultWindowsProperties(){
@@ -23,11 +36,6 @@ public class Window{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void setDefaultTableProperties(){
-        table.setBounds(20,110,1560,300);
-        table.setEnabled(false);
-    }
-
     private void setDefaultTitleProperties(){
         title.setBounds(0,20,1600,100);
         title.setForeground(Color.white);
@@ -36,15 +44,58 @@ public class Window{
         title.setFont(new Font("Mono", Font.BOLD, 36));
     }
 
+    private void setDefaultMensajesProperties(){
+        mensajeUltimo.setBounds(50,130,300,130);
+        mensajeUltimo.setForeground(Color.white);
+        mensajeUltimo.setFont(new Font("Mono",Font.BOLD,16));
+        mensajeCola.setBounds(50,130,300,90);
+        mensajeCola.setForeground(Color.white);
+        mensajeCola.setFont(new Font("Mono",Font.BOLD,16));
+        mensajeActual.setBounds(50,130,300,50);
+        mensajeActual.setForeground(Color.white);
+        mensajeActual.setFont(new Font("Mono",Font.BOLD,16));
+        procesoUltimo.setBounds(350,130,300,130);
+        procesoUltimo.setForeground(Color.white);
+        procesoUltimo.setFont(new Font("Mono",Font.BOLD,16));
+        procesoCola.setBounds(350,130,300,90);
+        procesoCola.setForeground(Color.white);
+        procesoCola.setFont(new Font("Mono",Font.BOLD,16));
+        procesoActual.setBounds(350,130,300,50);
+        procesoActual.setForeground(Color.white);
+        procesoActual.setFont(new Font("Mono",Font.BOLD,16));
+    }
+
+    private void setDefaultCalculosProperties(){
+        calculos.setEnabled(false);
+        calculos.setBounds(50,420,1500,400);
+        calculos.setRowHeight(50);
+        calculos.setPreferredScrollableViewportSize(new Dimension(1500,400));
+        calculos.setFillsViewportHeight(true);
+        calculos.setValueAt("Proceso",0,0);
+        calculos.setValueAt("Tiempo Llegada",0,1);
+        calculos.setValueAt("Duración de Ráfaga",0,2);
+        calculos.setValueAt("Tiempo de Arranque",0,3);
+        calculos.setValueAt("Tiempo de Finalización",0,4);
+        calculos.setValueAt("Tiempo de Retorno",0,5);
+        calculos.setValueAt("Tiempo de Respuesta",0,6);
+        calculos.setValueAt("Tasa de Desperdicio",0,7);
+        calculos.setValueAt("Tasa de Penalización",0,8);
+        calculos.setValueAt("Tiempo de Espera",0,9);
+    }
+
     private void addWindowsElements(){
         window.add(title);
-        window.add(table);
+        window.add(mensajeUltimo);
+        window.add(mensajeCola);
+        window.add(mensajeActual);
+        window.add(calculos);
     }
 
     public void createWindow(){
         setDefaultWindowsProperties();
-        setDefaultTableProperties();
         setDefaultTitleProperties();
+        setDefaultMensajesProperties();
+        setDefaultCalculosProperties();
         addWindowsElements();
     }
 }
