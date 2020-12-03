b4f6bb5643714cefbda6e76e25e24254e795962f
darkskoll <erick.lievana@gmail.com>
2020-12-02 22:29:15 -0600

    remade simulator part

--- Runable.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class Runable{
-    public Process current;
-    public Simulator simulator;
-
-    public abstract ArrayList<Results> run();
-    public abstract void queue(Process process);
-}
